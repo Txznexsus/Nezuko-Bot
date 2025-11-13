77a96b6a11d4fda3fc34ad43f82d22de00096924
Txznexsus <draxxzzproyectos@gmail.com>
2025-11-13 10:13:59 -0500

    main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -129,8 +129,8 @@ let handler = async (m, { conn, usedPrefix, __dirname, participants }) => {
     const cuerpo = infoUser + `*🧃 mᥱᥒᥙ ძіs⍴᥆ᥒіᑲᥣᥱ 🍁*${menuTexto}`.trim()
 
     const imgs = [
-      'https://qu.ax/AiJcY.jpg',
-      'https://qu.ax/Ggado.jpg'
+      'https://files.catbox.moe/l4l40l.jpg',
+      'https://files.catbox.moe/l4l40l.jpg'
     ]
     let imageUrl = imgs[Math.floor(Math.random() * imgs.length)]
 
