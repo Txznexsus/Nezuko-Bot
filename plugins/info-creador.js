b0234f209bc2afbe39ce47f3e6d0529c4ff05027
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-11-16 21:02:11 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,4 +1,4 @@
-/*
+
 // by dv.shadow - https://github.com/Yuji-XDev
 import PhoneNumber from 'awesome-phonenumber';
 
@@ -51,85 +51,6 @@ END:VCARD`.trim();
       }
     }
   }, { quoted: fkontak });
-};
-
-handler.help = ['creador'];
-handler.tags = ['info'];
-handler.command = ['creador', 'creator', 'owner'];
-export default handler;*/
-
-// by dv.shadow - https://github.com/Yuji-XDev
-import baileys from '@whiskeysockets/baileys'
-const { proto } = baileys
-import PhoneNumber from 'awesome-phonenumber';
-
-const handler = async (m, { conn }) => {
-  const name = 'sһᥲძ᥆ᥕ-᥊ᥡz | ᥆𝖿𝖿іᥴіᥲᥩ'
-  const numCreador = '51919199620'
-  const empresa = 'ᴋᴀɴᴇᴋɪ ʙᴏᴛ ɪɴɪᴄ.'
-  const about = '🍃 𝑫𝒆𝒔𝒂𝒓𝒓𝒐𝒍𝒍𝒂𝒅𝒐𝒓 𝒐𝒇𝒇𝒊𝒄𝒊𝒂𝒍 𝒅𝒆 𝑲𝒂𝒏𝒆𝒌𝒊-𝑩𝒐𝒕 𝑽3'
-  const correo = 'shadowcore.xyz@gmail.com'
-  const web = 'https://shadow-xyz.vercel.app/'
-  const direccion = 'Tokyo, Japón 🇯🇵'
-  const fotoPerfil = 'https://qu.ax/tAWKZ.jpg'
-
-  const vcard = `
-BEGIN:VCARD
-VERSION:3.0
-N:;${name};;;
-FN:${name}
-ORG:${empresa}
-TITLE:CEO & Fundador
-TEL;waid=${numCreador}:${new PhoneNumber('+' + numCreador).getNumber('international')}
-EMAIL:${correo}
-URL:${web}
-NOTE:${about}
-ADR:;;${direccion};;;;
-X-ABADR:ES
-X-WA-BIZ-NAME:${name}
-X-WA-BIZ-DESCRIPTION:${about}
-END:VCARD`.trim();
-
-  const contactMessage = {
-    displayName: name,
-    vcard
-  };
-
-  await m.react('🌿')
-
-  const msg = {
-    contacts: {
-      displayName: name,
-      contacts: [contactMessage]
-    },
-
-    contextInfo: {
-      mentionedJid: [m.sender],
-      externalAdReply: {
-        title: '🍃 Contacto de mi creador 🍉',
-        body: 'Kaneki Bot Oficial',
-        mediaType: 1,
-        thumbnailUrl: fotoPerfil,
-        renderLargerThumbnail: true,
-        sourceUrl: web
-      },
-      productMessage: {
-        product: {
-          productImage: { url: fotoPerfil },
-          productId: '7777777777',
-          title: '🍃 ᴄᴏɴᴛᴀᴄᴛᴏ ᴅᴇ ᴍɪ ᴄʀᴇᴀᴅᴏʀ 🍉',
-          description: `uwu`,
-          currencyCode: 'USD',
-          priceAmount1000: '100000',
-          retailerId: 666,
-          url: 'https://wa.me/' + numCreador,
-          productImageCount: 1,
-        }
-      }
-    }
-  }
-
-  await conn.sendMessage(m.chat, msg, { quoted: m })
 };
 
 handler.help = ['creador'];
