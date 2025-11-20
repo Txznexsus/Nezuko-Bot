99073b5d83a83de235f1af3a47b5d75146b6b617
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-11-19 20:29:09 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -6,6 +6,7 @@ import fs, { unwatchFile, watchFile } from "fs"
 import chalk from "chalk"
 import fetch from "node-fetch"
 import ws from "ws"
+import './test.js'
 
 const { proto } = (await import("@whiskeysockets/baileys")).default
 const isNumber = x => typeof x === "number" && !isNaN(x)
