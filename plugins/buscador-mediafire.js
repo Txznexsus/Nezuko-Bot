7d7826d06ce8dcbe344048bee43d7906f7eb85d8
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-11-22 14:25:13 -0500

    Actualizar buscador-mediafire.js

--- plugins/buscador-mediafire.js
+++ plugins/buscador-mediafire.js
@@ -1,7 +1,22 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return conn.reply(m.chat, `*⚡ Usa el comando así:*\n\n> ${usedPrefix + command} Dragon Ball`, m, rcanal)
+
+  if (!text) 
+    return conn.reply(
+      m.chat, 
+      `*⚡ Usa el comando así:*\n\n> ${usedPrefix + command} Dragon Ball`, 
+      m, 
+      rcanal
+    )
+
+  // MINI IMAGEN DE CARGA
+  let carga = "https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/1763676293102_924672.jpeg"
+
+  await conn.sendMessage(m.chat, {
+    image: { url: carga },
+    caption: `🔍 *Buscando resultados para:* ${text}\n*⏳ Espera un momento...*`
+  }, { quoted: m })
 
   await m.react('🕐')
 
@@ -10,31 +25,38 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let res = await fetch(`https://api.stellarwa.xyz/search/mediafire?query=${encodeURIComponent(text)}&key=stellar-3j2706f1`)
     let json = await res.json()
 
-    if (!json || !json.results || json.results.length === 0) {
-      throw `No se encontraron resultados para: *${text}*`
+    if (!json?.results?.length) {
+      await m.react('❌')
+      return conn.reply(m.chat, `⚠️ No se encontraron resultados para: *${text}*`, m)
     }
 
-    let txt = `╭━━━━━━━╮
-┋ ʀᴇsᴜʟᴛᴀᴅᴏs
-┋   🦌 ᴅᴇ 🎅
-┋ ᴍᴇᴅɪᴀғɪʀᴇ
-┋ ʙᴜsǫᴜᴇᴅᴀ: ${text}
-╰━━━━━━━╯`
+    let txt = `
+╭━━━━━━ ⪻🦌⪼ ━━━━━━╮
+   *RESULTADOS MEDiAFiRE*
+   📌 *Búsqueda:* ${text}
+╰━━━━━━ ⪻🎅⪼ ━━━━━━╯
+    `.trim() + "\n\n"
 
-    json.results.forEach((file, i) => {
-      txt += `🎋 *${i + 1}.*\n*${file.filename || 'Archivo desconocido'}*\n`
-      txt += `⚖️ *Tamaño:* ${file.filesize || 'Desconocido'}\n`
-      txt += `🌾 *Link:* ${file.url || 'No disponible'}\n`
-      txt += `🍃 *URL Fuente:* ${file.source_url || 'No disponible'}\n`
-      txt += `❄️ *Título Fuente:* ${file.source_title || 'Sin título'}\n\n\n`
+    json.results.forEach((f, i) => {
+      txt += `
+*${i + 1}. ${f.filename || 'Archivo desconocido'}*
+📦 *Tamaño:* ${f.filesize || 'Desconocido'}
+🔗 *Link:* ${f.url || 'No disponible'}
+🌐 *Fuente:* ${f.source_url || 'No disponible'}
+📘 *Título:* ${f.source_title || 'Sin Título'}
+
+──────────────────────
+`
     })
 
     await m.react('✔️')
-    await conn.reply(m.chat, txt.trim(), m, rcanal)
 
-  } catch (err) {
-    console.error(err)
-    await conn.reply(m.chat, '*Error al consultar la API de MediaFire.*', m)
+    await conn.reply(m.chat, txt, m, rcanal)
+
+  } catch (e) {
+    console.error(e)
+    await m.react('💀')
+    conn.reply(m.chat, `❌ *Error al consultar la API.*`, m)
   }
 }
 
