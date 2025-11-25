109b1fa81d365457f68493a1e1fbb0c239e16ece
Txznexsus <draxxzzproyectos@gmail.com>
2025-11-25 10:34:11 -0500

    main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -76,7 +76,7 @@ let handler = async (m, { conn, usedPrefix, __dirname, participants }) => {
       }
     }
 
-    const infoUser = `    🌳 •┆ 𝐊𝐀𝐍𝐄𝐊𝐈 𝐁𝐎𝐓 𝐀𝐈 ┆• 🥙
+    const infoUser = `    🍃┆һ᥆ᥣᥲ s᥆ᥡ nezuko-bot┆🪵
           •╌╌╌╌╌╌╌╌╌╌╌╌•
  ִ ࣪ 𓈒 ᗣ  ${ucapan()}  ࣫ㅤׅ 🎄۫ 
     ᗞᗞ @${userId}  ⌒᷼🥗 
