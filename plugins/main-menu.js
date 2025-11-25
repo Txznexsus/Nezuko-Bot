1c7c49bc28b3989feddbd0a25021969eb38a9864
Txznexsus <draxxzzproyectos@gmail.com>
2025-11-25 11:40:57 -0500

    main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -69,7 +69,7 @@ let handler = async (m, { conn, usedPrefix, __dirname, participants }) => {
     for (let tag in tags) {
       let comandos = commands
         .filter(cmd => cmd.tags.includes(tag))
-        .map(cmd => cmd.help.map(e => `* > ${usedPrefix}${e}*`).join('\n'))
+        .map(cmd => cmd.help.map(e => `*> ${usedPrefix}${e}*`).join('\n'))
         .join('\n')
       if (comandos) {
         menuTexto += `\n\n*╭──꒰* ${tags[tag]} *꒱𔖲𔖮𔖭*\n${comandos}\n*╰─────────────┈┄╌*\n`
